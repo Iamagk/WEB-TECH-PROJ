752d1866267577622be84edef53ab660fc2db28c
akshayphuatne18 <akshayphutane8@gmail.com>
2024-12-29 11:04:22 +0530

    middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,7 @@
-const { PrismaClient } = require('@prisma/client');
+const express = require('express');
 
-const prisma = new PrismaClient();
-
-async function main() {
-  // Create a new user
-  const newUser = await prisma.user.create({
-    data: {
-      name: 'John Doe',
-      email: 'john.doe@example.com',
-    },
-  });
-  console.log('Created User:', newUser);
-
-  // Fetch all users
-  const users = await prisma.user.findMany();
-  console.log('All Users:', users);
-}
-async function main() {
-  // Create a new user
-  const newUser = await prisma.student.create({
-    data: {
-      usn:16,
-      name:"akshay",
-      email: 'akshay@gmail.com',
-    },
-  });
-  console.log('Created User:', newUser);
-
-  // Fetch all users
-  const users = await prisma.user.findMany();
-  console.log('All Users:', users);
-}
-main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+const app = express();
+const PORT =8000
+app.listen(PORT,() =>{
+    console.log(`server running at http://localhost:${PORT}`);
+});
