e5d695c7657c31009fc6e14ca32b9f860190c9f9
Iamagk <allenngeorgek@gmail.com>
2025-04-02 16:03:45 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import { ThreeDots } from "react-loader-spinner";
 import AuthPage from "./pages/AuthPage";
